1067113ca9e403ac5fd7c32a42dea17cad745e1c
mario8a <mario8ato@hotmail.com>
2020-12-09 19:42:18 -0600

    Configuracion de sockets server

--- classes/server.ts
+++ classes/server.ts
@@ -1,19 +1,49 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
+import socketIO from 'socket.io';
+import http from 'http';
+
+import * as socket from '../sockets/socket';
 
 export default class Server {
+
+   private static _instance: Server;
 
    public app: express.Application;
    public port: number;
 
-   constructor() {
+   public io: socketIO.Server;
+   private httpSever: http.Server;
+
+   private constructor() {
       this.app = express();
       this.port = SERVER_PORT;
+
+      this.httpSever = new http.Server(this.app);
+      this.io = socketIO(this.httpSever);
+      this.escucharSockets();
+   }
+
+   public static get instance() {
+      return this._instance || (this._instance = new this());
+   }
+
+   private escucharSockets() {
+      console.log('Escuchando conexiones');
+
+      this.io.on('connection', cliente => {
+         console.log('Cliente conectado');
+
+         //mensajes
+         socket.mensaje(cliente, this.io);
+
+         socket.desconectar(cliente);
+      });
    }
 
    //metodo para inicializar el servidor
    start(callback: any) {
-      this.app.listen(this.port, callback)
+      this.httpSever.listen(this.port, callback)
    }
 
 }
---
+++ sockets/socket.ts
@@ -0,0 +1,23 @@
+//Configuracion de cada una de las acciones
+
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+
+export const desconectar = (cliente: Socket) => {
+
+   cliente.on('disconnect', () => {
+      console.log('Cliente desconectado')
+   });
+}
+
+//Escuchar mensajes del cliente
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+   cliente.on('mensaje', (payload: {de: string, cuerpo: string}) => {
+
+      console.log('Mensaje recibido', payload);
+
+      io.emit('mensaje-nuevo', payload);
+
+   })
+}
